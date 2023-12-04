8ff64944186dbbc974c2b06bed8d550a512c9463
pappik <pappik@gmail.com>
2023-12-04 18:06:01 -0500

    jenkins

---
+++ vars/ci.groovy
@@ -0,0 +1,33 @@
+def call() {
+    pipeline {
+        agent {
+            label 'ansible'
+        }
+
+        stages {
+
+            stage('Build/Compile'){
+                steps {
+                    echo 'build'
+                }
+            }
+
+            stage('Unit Test') {
+                steps{
+                    echo 'unit test'
+                }
+            }
+
+            stage('Quality control'){
+                steps {
+                    echo 'quality control'
+                }
+            }
+
+        }
+        post {
+
+        }
+
+    }
+}
