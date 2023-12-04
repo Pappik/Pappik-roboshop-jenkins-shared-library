0014822bfc2d321264050806ce0bfc896807d074
pappik <pappik@gmail.com>
2023-12-04 18:45:21 -0500

    jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -8,7 +8,7 @@ def call() {
 
             stage('Build/Compile'){
                 steps {
-                    echo 'build'
+                    common.compile()
                 }
             }
 
