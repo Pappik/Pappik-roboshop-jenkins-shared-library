222cb15589d4f5cee71d16704d4058fe5eea7801
pappik <pappik@gmail.com>
2023-12-05 22:45:42 -0500

    jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -4,7 +4,7 @@ def call() {
 
             stage('Checkout') {
                 cleanWs()
-                git branch: 'main', url: 'https://github.com/Pappik/catalogue'
+                git branch: 'main', url: "https://github.com/Pappik/${component}"
             }
 
             stage('Compile/Build') {
