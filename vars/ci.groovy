9de6dc52f270ef4a8e8eaa5cfa9e482bf17ad0bf
pappik <pappik@gmail.com>
2023-12-05 22:12:54 -0500

    jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,54 +1,37 @@
 def call() {
     try {
-        pipeline {
-            agent {
-                label 'workstation'
+        node('workstation') {
+
+            stage('Checkout') {
+                cleanWs()
+                git branch: 'main', url: 'https://github.com/Pappik/cart'
             }
 
-            stages {
-
-                stage('Build/Compile') {
-                    steps {
-                        script {
-
-                            common.compile()
-                        }
-                    }
-                }
-
-                stage('Unit Test') {
-                    steps {
-                        script {
-
-                            common.unittests()
-                        }
-                    }
-                }
-
-                stage('Quality control') {
-                    environment {
-                        SONAR_USER= '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
-                        SONAR_PASS= '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption  --query Parameters[0].Value| sed \'s/"//g\')'
-                    }
-                    steps {
-                        script {
-                            //sh "echo ${SONAR_USER}; echo "sonar" "
-                            sh "sonar-scanner -Dsonar.host.url=http://172.31.38.43:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
-                        }
-                    }
-                }
-
-                stage('upload code to central repo') {
-                    steps {
-                        echo 'Upload'
-                    }
-                }
-
+            stage('Compile/Build') {
+                common.compile()
             }
 
 
+            stage('Unit Test') {
+                common.unittests()
+            }
+
+            stage('Quality control') {
+                SONAR_USER = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
+                    sh "sonar-scanner -Dsonar.host.url=http://172.31.38.43:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                }
+            }
         }
-    } catch(Exception e) {
+
+        stage('upload code to central repo') {
+            steps {
+                echo 'Upload'
+            }
+        }
+
+    } catch (Exception e) {
         common.email("Failed")
     }
 }
