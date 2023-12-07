d9fd80619b1baa21a24707af7b27979dfd1d41ad
pappik <pappik@gmail.com>
2023-12-07 17:05:34 -0500

    jenkins

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -4,18 +4,15 @@ def call() {
             label 'workstation'
         }
 
-//        parameters {
-//            string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
-//        }
+        parameters {
+            string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
+        }
 
         stages {
             stage(' init') {
                 steps {
-                    script {
-                        sh 'echo hello'
-                    }
 
-                   // sh 'terraform init -backend-config=env-${INFRA_ENV}/state.tfvars'
+                    sh 'terraform init -backend-config=env-${INFRA_ENV}/state.tfvars'
                 }
             }
         }
